97d60092470c4aaf6d8d2bec9e3df341a6e04897
ha-da-ra <gold85600@gmail.com>
2024-06-14 21:44:51 +0200

    add simple start page

--- mathBattle/client/src/StartMenu.tsx
+++ mathBattle/client/src/StartMenu.tsx
@@ -1,12 +1,31 @@
-import { Button } from '@mui/material'
+import { Box, Button, Grid, Typography } from '@mui/material'
 import { useState } from 'react'
+
 function StartMenu() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Button variant="contained">Hello world</Button>
-    </>
+    <Box textAlign='center' sx={{m: 10}}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h2" >
+            Welcome
+          </Typography><br/>
+          <Typography variant="h4" >
+            this is
+          </Typography><br/>
+          <Typography variant="h2" >
+            Math Battle
+          </Typography><br/>
+        </Grid>
+        <Grid item xs={12}>
+          <Button sx={{ alignSelf: "center" }} variant="contained">Login</Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button sx={{ alignSelf: "center" }} variant="outlined">Register</Button>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
--- mathBattle/client/src/main.tsx
+++ mathBattle/client/src/main.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import StartMenu from './StartMenu.tsx'
+import { ThemeProvider, createTheme } from '@mui/material';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4648ba',
+    },
+    secondary: {
+      main: '#f3bc77',
+    }
+  },
+  typography: {
+    fontFamily: "Lora, sans-serif",
+    fontSize:16
+  }
+});
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <StartMenu />
+    <ThemeProvider theme={theme}>
+    <StartMenu /></ThemeProvider>
   </React.StrictMode>,
 )

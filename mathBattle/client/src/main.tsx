83d689360cf82070e7c476fe961d25d5ee429d1f
Thomas <t.dluhosch@gmail.com>
2024-06-18 10:51:36 +0200

    created LevelSucceed page

--- mathBattle/client/src/main.tsx
+++ mathBattle/client/src/main.tsx
@@ -1,34 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { LoginPage } from './Login/LoginPage.tsx'
-import { ThemeProvider } from '@mui/material';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { theme } from './main-theme.ts';
-import { LevelMapPage } from './LevelMap/LevelMapPage.tsx';
-import { NotFound } from './NotFound.tsx';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { LoginPage } from "./Login/LoginPage.tsx";
+import { ThemeProvider } from "@mui/material";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { theme } from "./main-theme.ts";
+import { LevelMapPage } from "./LevelMap/LevelMapPage.tsx";
+import { NotFound } from "./NotFound.tsx";
+import { LevelSucceed } from "./LevelMap/LevelSucceed.tsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LoginPage/>,
-  },
-  {
-    path: "/map",
-    element: <LevelMapPage name='John' levels={[{number: 1, completed: true}, {number: 2, completed: true}, {number: 3, completed: false}]}/>,
-  },{
-    path: "*",
-    element: <NotFound/>,
-  },
+    {
+        path: "/",
+        element: <LoginPage />,
+    },
+    {
+        path: "/succeed",
+        element: <LevelSucceed />,
+    },
+    {
+        path: "/map",
+        element: (
+            <LevelMapPage
+                name="John"
+                levels={[
+                    { number: 1, completed: true },
+                    { number: 2, completed: true },
+                    { number: 3, completed: false },
+                ]}
+            />
+        ),
+    },
+    {
+        path: "*",
+        element: <NotFound />,
+    },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ThemeProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <RouterProvider router={router} />
+        </ThemeProvider>
+    </React.StrictMode>
+);

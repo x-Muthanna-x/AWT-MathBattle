d0bba90d5d31aefe2ea00cb42306ba0c3a0c4b11
Thomas <t.dluhosch@gmail.com>
2024-06-19 15:30:30 +0200

    added styling to LevelSucceed

--- mathBattle/client/src/LevelMap/LevelSucceed.tsx
+++ mathBattle/client/src/LevelMap/LevelSucceed.tsx
@@ -1,22 +1,29 @@
-import {
-    Card,
-    CardContent,
-    Typography,
-    CardActions,
-    Box,
-    Button,
-} from "@mui/material";
+import { Typography, Box, Button, Grid, ButtonGroup } from "@mui/material";
 import { NavBar } from "../NavBar";
 
 export function LevelSucceed() {
     return (
         <div>
             <NavBar />
-            You did great!
-            <div>You answerd # equations correct!</div>
-            <div>
-                <Button>Proceed</Button>
-            </div>
+
+            <Box textAlign="center" sx={{ m: 10 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Typography variant="h1">Well done!</Typography>
+                        <Typography variant="h6">
+                            You solved this level in just x seconds!
+                        </Typography>
+                        <Typography variant="h6">
+                            # equations were correct!
+                        </Typography>
+                    </Grid>
+
+                    <Grid container spacing={2}>
+                        <Button variant="outlined">Main Menu</Button>
+                        <Button variant="contained">Next Level</Button>
+                    </Grid>
+                </Grid>
+            </Box>
         </div>
     );
 }
